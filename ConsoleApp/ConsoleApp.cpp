904786355b80627a64cc3ce7e7e130a34b020aaf
kgp_kgp_kgp <kgp_kgp_kgp@mail.zp.ua>
2023-03-31 10:30:27 +0300

    yet another string added

--- ConsoleApp/ConsoleApp.cpp
+++ ConsoleApp/ConsoleApp.cpp
@@ -8,6 +8,8 @@ int main()
     std::cout << "Hello World!\n";
 
     std::cout << "Another String!\n";
+
+    std::cout << "Yet nother String!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
