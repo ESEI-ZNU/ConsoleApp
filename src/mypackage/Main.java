c693ed0d34ed892a691595881bdc1e792e2fc9bf
kolgenpav <kolgenpav@gmail.com>
2026-03-26 09:09:57 +0200

    Initial commit.

---
+++ src/mypackage/Main.java
@@ -0,0 +1,18 @@
+package mypackage;
+
+/**
+ * This is the main class of the application with entry point.
+ *
+ * @author Enter_Your_Name_Here
+ * @version 1.0
+ */
+public class Main {
+    /**
+     * The main method serves as the entry point of the application.
+     *
+     * @param args Command-line arguments passed to the application.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
