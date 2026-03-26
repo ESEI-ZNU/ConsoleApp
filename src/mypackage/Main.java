0f275ebac615ea1abbc2b52a9e32a718d60f9a44
kolgenpav <kolgenpav@gmail.com>
2026-03-26 13:02:22 +0200

    Message was added by Developer 1 to newMethod.

--- src/mypackage/Main.java
+++ src/mypackage/Main.java
@@ -22,5 +22,6 @@ public class Main {
      */
     private static void newMethod() {
         System.out.println("New method called!");
+        System.out.println("This was added by Developer 1.");
     }
 }
