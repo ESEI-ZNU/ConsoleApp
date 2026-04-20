e4ccaa0472185c4a825c81f2c4ce6611f441f1d1
kolgenpav <kolgenpav@gmail.com>
2026-04-20 12:25:32 +0300

    AnotherNewClass was added to branch feature1.

---
+++ src/mypackage/AnotherNewClass.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+public class AnotherNewClass {
+    /**
+     * This method demonstrates functionality in AnotherNewClass.
+     */
+    public void demonstrate() {
+        System.out.println("Demonstrating functionality in AnotherNewClass.");
+    }
+}
